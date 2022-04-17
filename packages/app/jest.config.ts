7c11e6168da11dd41eea247a06cc16f5ee7d11b4
Fahad Ahmed <contact@fhdamd.dev>
2022-04-17 13:35:05 +1000

    #3 setting up jest to handle different file imports like svg, images and font files

--- packages/app/jest.config.ts
+++ packages/app/jest.config.ts
@@ -1,5 +1,9 @@
 export default {
+  testEnvironment: 'jsdom',
   transform: {
     '^.+\\.(ts|tsx)?$': 'ts-jest',
+  },
+  moduleNameMapper: {
+    '\\.(gif|ttf|eot|svg|png)$': '<rootDir>/test/__mocks__/fileMock.js',
   },
 }
