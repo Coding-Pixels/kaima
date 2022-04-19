dc4793be46a6d6327fec196374dc4bc7d0440e26
Fahad Ahmed <contact@fhdamd.dev>
2022-04-19 21:45:06 +1000

    KAIM-1 [component-update]: added routes to sign-up and sign-in

--- packages/app/src/pages/routes.tsx
+++ packages/app/src/pages/routes.tsx
@@ -1,7 +1,25 @@
 import React from 'react'
+import {
+  BrowserRouter as Router,
+  Route,
+  Routes,
+  Navigate,
+} from 'react-router-dom'
+
+import { Register } from '.'
+
+const Login = () => <div>Login Component</div>
 
 function AppRoutes() {
-  return <div>App Routes Component</div>
+  return (
+    <Router>
+      <Routes>
+        <Route path="/sign-up" element={<Register />} />
+        <Route path="/sign-in" element={<Login />} />
+        <Route path="/" element={<Navigate to="/sign-in" />} />
+      </Routes>
+    </Router>
+  )
 }
 
 export default AppRoutes
