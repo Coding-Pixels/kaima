b619761d2e979b0fd826fe7fbd32b2fc0070c7ef
Fahad Ahmed <contact@fhdamd.dev>
2022-04-20 05:32:36 +1000

    KAIM-1 [component update]: added global styles and fonts to the app

--- packages/app/src/main.tsx
+++ packages/app/src/main.tsx
@@ -1,9 +1,19 @@
 import React from 'react'
 import ReactDOM from 'react-dom/client'
+import { Global, css } from '@emotion/react'
 import AppRoutes from './pages/routes'
 
 ReactDOM.createRoot(document.getElementById('root')!).render(
   <React.StrictMode>
+    <Global
+      styles={css`
+        @import url('https://fonts.googleapis.com/css2?family=EB+Garamond:wght@400;800&family=Montserrat:wght@400;500;700;800&display=swap');
+        html,
+        body {
+          background-color: #efefef;
+        }
+      `}
+    />
     <AppRoutes />
   </React.StrictMode>
 )
