335198b466cf0fb2de79519a8b6767461e0253a3
Fahad Ahmed <contact@fhdamd.dev>
2022-04-20 06:36:47 +1000

    KAIM-1 [component-update]: added a local PageSubTitle component in Register page

--- packages/app/src/pages/Register/Register.tsx
+++ packages/app/src/pages/Register/Register.tsx
@@ -1,10 +1,20 @@
 import React from 'react'
+import styled from '@emotion/styled'
 import { AppContainer, PageHeader } from '../../components'
 
+const PageSubTitle = styled.h2`
+  font-family: 'Montserrat', sans-serif;
+  font-weight: 400;
+  font-size: 40px;
+  margin: 0;
+`
 function Register() {
   return (
     <AppContainer>
-      <PageHeader>Ka&apos;ima</PageHeader>
+      <div>
+        <PageHeader>Ka&apos;ima</PageHeader>
+        <PageSubTitle>Register New User</PageSubTitle>
+      </div>
     </AppContainer>
   )
 }
