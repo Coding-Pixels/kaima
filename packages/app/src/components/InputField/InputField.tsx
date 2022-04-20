95a5d94e1d3dc944e02e774b49170a0166501f99
Fahad Ahmed <contact@fhdamd.dev>
2022-04-20 23:17:24 +1000

    KAIM-1 [component-update]: added styles to the InputField component

--- packages/app/src/components/InputField/InputField.tsx
+++ packages/app/src/components/InputField/InputField.tsx
@@ -1,12 +1,42 @@
 import React from 'react'
+import styled from '@emotion/styled'
+
+const StyledInput = styled.input`
+  font-family: 'EB Garamond', serif;
+  font-weight: 400;
+  font-size: 24px;
+  height: 48px;
+  width: 560px;
+  margin-bottom: 24px;
+  padding: 12px;
+  border: none;
+`
+const InputContainer = styled.div`
+  display: flex;
+  flex-direction: column;
+  align-items: start;
+`
+const StyledLabel = styled.label`
+  font-family: 'Montserrat', sans-serif;
+  font-weight: 400;
+  font-size: 16px;
+  margin-bottom: 12px;
+`
 
 type Props = {
   type: string
   name: string
   placeholder?: string
+  labelString?: string
 }
-function InputField({ type, name, placeholder }: Props) {
-  return <input type={type} name={name} placeholder={placeholder || ''} />
+
+function InputField({ type, name, placeholder, labelString }: Props) {
+  return (
+    <InputContainer>
+      <StyledLabel htmlFor={name}>{labelString}</StyledLabel>
+      <StyledInput type={type} name={name} placeholder={placeholder || ''} />
+    </InputContainer>
+  )
 }
 
 export default InputField
