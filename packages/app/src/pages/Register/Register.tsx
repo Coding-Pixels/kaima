1b00ca2555a6862c8eb73424b8cc2b4a790e536c
Fahad Ahmed <contact@fhdamd.dev>
2022-04-23 10:07:33 +1000

    KAIM-1 [component-update]: updated Register page to use Firebase Auth to create a new user using the page form

--- packages/app/src/pages/Register/Register.tsx
+++ packages/app/src/pages/Register/Register.tsx
@@ -1,7 +1,15 @@
-import React from 'react'
-import { Link } from 'react-router-dom'
+import React, { useState, useContext, SyntheticEvent } from 'react'
+import { Link, Navigate } from 'react-router-dom'
 import styled from '@emotion/styled'
-import { AppContainer, Button, InputField, PageHeader } from '../../components'
+import { AppContext, Button, InputField, PageHeader } from '../../components'
+import {
+  auth,
+  createUserWithEmailAndPassword,
+  updateProfile,
+  db,
+  doc,
+  setDoc,
+} from '../../config/firebase'
 
 const PageSubTitle = styled.h2`
   font-family: 'Montserrat', sans-serif;
@@ -23,47 +31,75 @@ const StyledLink = styled(Link)`
   font-family: 'Montserrat', sans-serif;
   font-weight: 400;
   font-size: 16px;
+
+  &:focus {
+    outline: solid 2px #626367;
+    border-radius: 4px;
+  }
 `
 function Register() {
-  const handleSubmit = (e: React.SyntheticEvent) => {
+  const [error, setError] = useState(null)
+  if (AppContext) {
+    const { currentUser } = useContext(AppContext)
+    if (currentUser) {
+      return <Navigate to="/dashboard" />
+    }
+  }
+  const handleSubmit = (e: SyntheticEvent) => {
     e.preventDefault()
     console.log(e)
+    const target = e.target as typeof e.target & {
+      name: { value: string }
+      email: { value: string }
+      password: { value: string }
+    }
+
+    const name = target.name.value
+    const email = target.email.value
+    const password = target.password.value
+
+    createUserWithEmailAndPassword(auth, email, password)
+      .then(async (result) => {
+        updateProfile(result.user, {
+          displayName: name,
+        })
+        await setDoc(doc(db, 'userData', result.user.uid), {
+          list: [],
+        })
+      })
+      .catch((err) => {
+        console.log(err)
+        setError(err.message)
+      })
   }
   return (
-    <AppContainer>
-      <div>
-        <PageHeader>Ka&apos;ima</PageHeader>
-        <PageSubTitle>Register New User</PageSubTitle>
-        <StyledForm onSubmit={handleSubmit}>
-          <InputField
-            type="text"
-            name="name"
-            placeholder="Your name"
-            labelString="Name"
-          />
-          <InputField
-            type="text"
-            name="email"
-            placeholder="Your email"
-            labelString="Email"
-          />
-          <InputField
-            type="password"
-            name="password"
-            placeholder="Your password"
-            labelString="Password"
-          />
-          <InputField
-            type="password"
-            name="retype-password"
-            placeholder="Re-type your password"
-            labelString="Re-type password"
-          />
-          <Button />
-          <StyledLink to="/sign-in">Returning User? Sign-in</StyledLink>
-        </StyledForm>
-      </div>
-    </AppContainer>
+    <div>
+      <PageHeader>Ka&apos;ima</PageHeader>
+      <PageSubTitle>Register New User</PageSubTitle>
+      <StyledForm onSubmit={handleSubmit}>
+        <InputField
+          type="text"
+          name="name"
+          placeholder="Your name"
+          labelString="Name"
+        />
+        <InputField
+          type="text"
+          name="email"
+          placeholder="Your email"
+          labelString="Email"
+        />
+        <InputField
+          type="password"
+          name="password"
+          placeholder="Your password"
+          labelString="Password"
+        />
+        <Button />
+        <StyledLink to="/sign-in">Returning User? Sign-in</StyledLink>
+        {error && <div>{error}</div>}
+      </StyledForm>
+    </div>
   )
 }
 
