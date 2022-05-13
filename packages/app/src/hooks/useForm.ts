97bf9eed62cab791322a00801c399a613c229c97
Fahad Ahmed <contact@fhdamd.dev>
2022-05-13 14:41:29 +1000

    KAIM-1 updated logic to the useForm and Register component to implement form submission

--- packages/app/src/hooks/useForm.ts
+++ packages/app/src/hooks/useForm.ts
@@ -20,6 +20,7 @@ type ValidateFieldProps = {
 const useForm = () => {
   const [data, setData] = useState(initialState)
   const [errors, setErrors] = useState(initialErrorState)
+  const [isFormValid, setIsFormValid] = useState(false)
 
   const handleChange = (e: ChangeEvent<HTMLInputElement>) => {
     setData((prevState) => ({
@@ -53,11 +54,32 @@ const useForm = () => {
     }
   }
 
-  const submitForm = () => {
-    console.log('This function is where you can submit the form')
+  const validateForm = () => {
+    console.log('This is where we validate if the form can be submitted or not')
+    let hasErrors = false
+    let containsEmptyFields = false
+    Object.keys(errors).forEach((e) => {
+      if (errors[e as keyof typeof initialErrorState] !== null) {
+        hasErrors = true
+      }
+    })
+    Object.keys(data).forEach((e) => {
+      if (data[e as keyof typeof initialState] === '') {
+        containsEmptyFields = true
+      }
+    })
+
+    if (!hasErrors && !containsEmptyFields) {
+      setIsFormValid(true)
+    }
   }
 
-  return { data, errors, handleChange, validateField, submitForm }
+  const submitForm = () => {
+    console.log('This function is where you can submit the form')
+    validateForm()
+  }
+
+  return { data, errors, handleChange, validateField, isFormValid, submitForm }
 }
 
 export default useForm
--- packages/app/src/pages/Register/Register.tsx
+++ packages/app/src/pages/Register/Register.tsx
@@ -18,10 +18,10 @@ import useForm from '../../hooks/useForm'
 // import registerForm from './formData.register.json';
 
 function Register() {
-  const { data, errors, handleChange, validateField } = useForm()
-  const [error, setError] = useState(null)
+  const { data, errors, handleChange, validateField, isFormValid, submitForm } =
+    useForm()
+  const [error, setError] = useState<string | null>(null)
 
-  console.log('errors', errors)
   if (AppContext) {
     const { currentUser } = useContext(AppContext)
     if (currentUser) {
@@ -30,30 +30,31 @@ function Register() {
   }
   const handleSubmit = (e: SyntheticEvent) => {
     e.preventDefault()
-    console.log(e)
-    const target = e.target as typeof e.target & {
-      name: { value: string }
-      email: { value: string }
-      password: { value: string }
+    submitForm()
+    if (isFormValid) {
+      console.log(data)
+
+      const name = data.name
+      const email = data.email
+      const password = data.password
+
+      createUserWithEmailAndPassword(auth, email, password)
+        .then(async (result) => {
+          updateProfile(result.user, {
+            displayName: name,
+          })
+          await setDoc(doc(db, 'userData', result.user.uid), {
+            list: [],
+          })
+        })
+        .catch((err) => {
+          console.log(err)
+          setError(err.message)
+        })
+      console.log('We should submit the form now')
+    } else {
+      setError('We cannot submit the form as there is an error')
     }
-
-    const name = target.name.value
-    const email = target.email.value
-    const password = target.password.value
-
-    createUserWithEmailAndPassword(auth, email, password)
-      .then(async (result) => {
-        updateProfile(result.user, {
-          displayName: name,
-        })
-        await setDoc(doc(db, 'userData', result.user.uid), {
-          list: [],
-        })
-      })
-      .catch((err) => {
-        console.log(err)
-        setError(err.message)
-      })
   }
 
   return (
@@ -87,6 +88,17 @@ function Register() {
           placeholder="Your email"
           labelString="Email"
           onChange={handleChange}
+          onBlur={() =>
+            validateField({
+              isRequired: true,
+              validationExpression:
+                '^[a-zA-Z0-9._-]+@[a-zA-Z0-9.-]+\\.[a-zA-Z]{2,4}$',
+              errorMessage: 'The email address entered is invalid',
+              value: data.email,
+              name: 'email',
+            })
+          }
+          error={errors.email}
         />
         <InputField
           value={data.password}
@@ -95,6 +107,17 @@ function Register() {
           placeholder="Your password"
           labelString="Password"
           onChange={handleChange}
+          onBlur={() =>
+            validateField({
+              isRequired: true,
+              validationExpression:
+                '^(?=.*[0-9])(?=.*[!@#$%^&*])[a-zA-Z0-9!@#$%^&*]{6,16}$',
+              errorMessage: 'Your password does not match the criteria',
+              value: data.password,
+              name: 'password',
+            })
+          }
+          error={errors.password}
         />
         <Button />
         <StyledLink to="/sign-in">Returning User? Sign-in</StyledLink>
