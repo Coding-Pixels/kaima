38d5ea64172508854cb7a80192f89b90d3d1be61
Fahad Ahmed <contact@fhdamd.dev>
2022-05-30 22:05:48 +1000

    #9 setup of eslint and prettier for the app package

---
+++ apps/app/.eslintrc.js
@@ -0,0 +1,61 @@
+module.exports = {
+  env: {
+    browser: true,
+    es2021: true,
+    jest: true,
+  },
+  extends: [
+    'eslint:recommended',
+    'plugin:react/recommended',
+    'plugin:@typescript-eslint/recommended',
+    'prettier',
+  ],
+  parser: '@typescript-eslint/parser',
+  parserOptions: {
+    ecmaFeatures: {
+      jsx: true,
+    },
+    ecmaVersion: 'latest',
+    sourceType: 'module',
+  },
+  plugins: ['react', '@typescript-eslint'],
+  rules: {
+    indent: ['error', 2],
+    'linebreak-style': ['error', 'unix'],
+    quotes: ['error', 'single'],
+    semi: ['error', 'always'],
+    'react/react-in-jsx-scope': 'off',
+    'no-duplicate-imports': 'error',
+    'no-var': 'error',
+    'brace-style': 'error',
+    'prefer-template': 'error',
+    radix: 'error',
+    'space-before-blocks': 'error',
+    'import/prefer-default-export': 'off',
+  },
+  overrides: [
+    {
+      files: [
+        '**/*.test.js',
+        '**/*.test.jsx',
+        '**/*.test.tsx',
+        '**/*.spec.js',
+        '**/*.spec.jsx',
+        '**/*.spec.tsx',
+      ],
+      env: {
+        jest: true,
+      },
+    },
+  ],
+  globals: {
+    window: true,
+    module: true,
+    process: true,
+  },
+  settings: {
+    'import/resolver': {
+      typescript: {},
+    },
+  },
+}
---
+++ apps/app/.prettierrc.js
@@ -0,0 +1,18 @@
+module.exports = {
+  semi: false,
+  tabWidth: 2,
+  printWidth: 80,
+  singleQuote: true,
+  trailingComma: 'all',
+  jsxSingleQuote: true,
+  bracketSpacing: true,
+  arrowParens: 'always',
+  overrides: [
+    {
+      files: '*.{js,jsx,tsx,ts,scss,json,html}',
+      options: {
+        tabWidth: 2,
+      },
+    },
+  ],
+}
