a93a91c7e8b54bba284c023a1f0fff4dc121d195
Fahad Ahmed <contact@fhdamd.dev>
2022-06-16 17:11:30 +1000

    #9 - added cloud function to add subscriber to mailchimp

--- apps/functions/src/index.ts
+++ apps/functions/src/index.ts
@@ -1,9 +1,31 @@
-import * as functions from "firebase-functions";
+/* eslint-disable indent */
+import * as functions from 'firebase-functions';
 
-// Start writing Firebase Functions
-// https://firebase.google.com/docs/functions/typescript
+import Mailchimp from 'mailchimp-api-v3';
 
 export const helloWorld = functions.https.onRequest((request, response) => {
-  functions.logger.info("Hello logs!", {structuredData: true});
-  response.send("Hello from Firebase!");
+  // eslint-disable-next-line object-curly-spacing
+  functions.logger.info('Hello logs!', { structuredData: true });
+  response.send('Hello from Firebase!');
+});
+
+const mailchimp = new Mailchimp(process.env.MAILCHIMP_API_KEY as string);
+
+export const addSubscriber = functions.https.onRequest((request, response) => {
+  mailchimp
+    .post('/lists/id/members', {
+      merge_fields: {
+        FNAME: request.body.fname,
+        LNAME: request.body.lname,
+      },
+      email_address: request.body.email,
+      status: 'subscribed',
+      tags: ['kaima_marketing_app'],
+    })
+    .then((results) => {
+      response.json(results);
+    })
+    .catch((err) => {
+      response.json(err);
+    });
 });
